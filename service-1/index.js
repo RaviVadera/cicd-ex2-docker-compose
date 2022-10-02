33f5789f86cf95042c9be21014c33a64c7b5a6a4
Ravi Vadera <61351969+RaviVadera@users.noreply.github.com>
2022-10-02 16:05:23 +0000

    Completed Service-1 and Service-2, Added Docker Compose Stack

---
+++ service-1/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import fetch from 'node-fetch';
+
+// read local ip address and port configuration
+// from environment variables
+const localIP = process.env.host || '0.0.0.0';
+const localPort = process.env.port || 3000;
+
+// configure express server
+const app = express();
+
+// '/' endpoint for service-1
+app.get('/', async (req, res) => {
+    // call service-2 endpoint
+    const service2Response = await (await fetch('http://service-2:3000/')).text();
+
+    // form and send response
+    const sender = `${req.socket.remoteAddress}:${req.socket.remotePort}`;
+    const receiver = `${req.socket.localAddress}:${req.socket.localPort}`;
+    let response = `Hello from ${sender}<br/>to ${receiver}<br/>${service2Response}`;
+    res.send(response);
+});
+
+// start express server on port
+app.listen(localPort, localIP, () => {
+    console.log("service-1 -> started");
+});
---
+++ service-2/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+
+// read local ip address and port configuration
+// from environment variables
+const localIP = process.env.host || '0.0.0.0';
+const localPort = process.env.port || 3000;
+
+// configure express server
+const app = express();
+
+// '/' endpoint for service-2
+app.get('/', (req, res) => {
+    // form and send response
+    // form and send response
+    const sender = `${req.socket.remoteAddress}:${req.socket.remotePort}`;
+    const receiver = `${req.socket.localAddress}:${req.socket.localPort}`;
+    let response = `Hello from ${sender}<br/>to ${receiver}`
+    res.send(response);
+});
+
+// start express server on port
+app.listen(localPort, localIP, () => {
+    console.log("service-2 -> started");
+});
